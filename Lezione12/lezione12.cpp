75c53b0f7d9c515567c8d7cd05ffad0faf2e90f6
gigiar02 <luigiarianopio02@gmail.com>
2025-01-24 22:46:54 +0100

    Lezione 12(Non finita)

---
+++ Lezione12/lezione12.cpp
@@ -0,0 +1,187 @@
+#include "lezione12.hpp"
+void ABR::visita()
+{
+    cout<<"Inizio visita anticipata: root -> sx -> dx"<<endl;
+    coutVisitaAnticipata(root);
+}
+void ABR::coutVisitaAnticipata(Node* root)
+{
+    if(root != nullptr)
+    {
+        cout<<" Dato = "<<root->getDato()<<endl;
+        Node* left = new Node();
+        if((left = root->getLeftChild())!= nullptr)
+        {
+            coutVisitaAnticipata(left);
+        }
+        Node* right = new Node();
+        if((right = root->getRightChild())!= nullptr)
+        {
+            coutVisitaAnticipata(right);
+        }
+
+    }
+}
+void ABR::addNode(int x)
+{
+    //Creo il nuovo nodo con valore x
+    Node* newNode = new Node();
+    newNode->setData(x);
+    //Creo un nodo  contenente la radice
+    Node* y = root;
+    //Conterrà di volta in volta un nodo all'interno dell'albero
+    Node* temp = nullptr;
+    //Cerco il posto in cui inserire il nodo
+    while(y != nullptr)
+    {
+        temp = y;
+        //Cerca a destra
+        if(x > y->getDato())
+        {
+            y = y->getRightChild();
+        }else
+        //Cerca a sinistra
+        {
+            y = y->getLeftChild();
+        }
+    }
+    if(root == nullptr)
+    {
+        root = newNode;
+    }else
+    {
+        newNode->setFather(temp);
+        if(x > temp->getDato())
+        {
+            temp->setRightChild(newNode);
+
+        }else
+        {
+            temp->setLeftChild(newNode);
+        }
+    }
+
+
+
+}
+
+Node* ABR::findSuccessor(Node* x)
+{
+    if(!x || !x->getRightChild())
+    {
+        return nullptr;
+    }
+    Node* y = x->getRightChild();
+    //Cerco il piu piccolo tra i piu grandi
+    while(y->getLeftChild())
+    {
+        y = y->getLeftChild();
+    }
+    return y;
+}
+void ABR::addNode(Node* newNode)
+{
+    int x = newNode->getDato();
+    //Creo un nodo  contenente la radice
+    Node* y = root;
+    //Conterrà di volta in volta un nodo all'interno dell'albero
+    Node* temp = nullptr;
+    //Cerco il posto in cui inserire il nodo
+    while(y != nullptr)
+    {
+        temp = y;
+        //Cerca a destra
+        if(x > y->getDato())
+        {
+            y = y->getRightChild();
+        }else
+        //Cerca a sinistra
+        {
+            y = y->getLeftChild();
+        }
+    }
+    if(root == nullptr)
+    {
+        root = newNode;
+    }else
+    {
+        newNode->setFather(temp);
+        if(x > temp->getDato())
+        {
+            temp->setRightChild(newNode);
+
+        }else
+        {
+            temp->setLeftChild(newNode);
+        }
+    }
+
+
+
+}
+//Elimina un nodo dall'albero
+void ABR::deleteNode(Node* x)
+{
+    Node* p = x->getFather();
+    Node* sx = x->getLeftChild();
+    Node* dx = x->getRightChild();
+    //Se il nodo è vuoto o non ha figli
+    if(!x || (!sx && !dx))
+    {
+        return;
+    }
+    //il nodo ha un solo figlio
+    if(sx && !dx)
+    {
+        //il figlio sinistro diventa figlio del padre dell' ex nodo x
+        sx->setFather(p);
+        if(p->getLeftChild() == x)
+        {
+            p->setLeftChild(sx);
+        }else if(p->getRightChild() == x)
+        {
+            p->setRightChild(sx);
+        }
+
+
+    }else if(!sx && dx)
+    {
+        //il figlio destro diventa figlio del padre dell' ex nodo x
+        dx->setFather(p);
+        if(p->getLeftChild() == x)
+        {
+            p->setLeftChild(dx);
+        }else if(p->getRightChild() == x)
+        {
+            p->setRightChild(dx);
+        }
+
+    }
+    //Il nodo ha sia figlio sx che dx
+    else
+    {
+        //Trova il successore
+        Node* y = findSuccessor(x);
+        if(y->getRightChild())
+        {
+            cout<<"successor ="<<y->getDato()<<endl;
+            deleteNode(y);
+        }
+        y->setFather(p);
+        y->setLeftChild(sx);
+        y->setRightChild(dx);
+        sx->setFather(y);
+        dx->setFather(y);
+        if(p->getLeftChild() == x)
+        {
+            p->setLeftChild(y);
+
+        }else
+        {
+            p->setRightChild(y);
+        }
+        delete x;
+    }
+}
+
+
---
+++ Lezione12/lezione12.hpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include "lezione16.hpp"
+#include <sstream>
+
+
+class Node
+{
+    //Data
+    int dato;
+
+    //Puntatori
+    Node *father;
+    Node *leftChild;
+    Node *rightChild;
+
+    //Costruttore
+    public:
+        Node(Node *father, Node *leftChild,Node *rightChild){this->father = father; this->leftChild = leftChild; this->rightChild = rightChild;}
+        Node(){this->father = nullptr; this->leftChild = nullptr; this->rightChild = nullptr;}
+
+    //Getters and Setters
+    int getDato(){return dato;}
+    Node* getLeftChild(){return leftChild;}
+    Node* getRightChild(){return rightChild;}
+    Node* getFather(){return father;}
+    void setFather(Node* newFather){father = newFather;}
+    void setLeftChild(Node* newChild){leftChild = newChild;}
+    void setRightChild(Node* newChild){rightChild = newChild;}
+    void setData(int newDato){dato = newDato;}
+};
+
+
+class ABR
+{
+    //Dati
+    Node* root;
+
+    //Costruttore
+    public:
+        ABR(){root = nullptr;}
+    //Metodi
+        void addNode(int x);
+        void addNode(Node* newNode);
+        //Visita root -> sx -> dx VisitaAnticipata
+        void visita();
+        void coutVisitaAnticipata(Node*root);
+        //Trova il successore
+        Node* findSuccessor(Node* x);
+        void deleteNode(Node* x);
+};
